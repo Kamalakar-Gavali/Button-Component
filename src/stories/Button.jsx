8424f3025c74cdbe73879f39bb8954354865caa4
Kamalakar <kamalakar@nssplindia.com.com>
2021-10-21 17:31:00 +0530

    done

--- src/stories/Button.jsx
+++ src/stories/Button.jsx
@@ -1,59 +1,66 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './button.css';
+import Icon from './Icon';
 
 /**
  * Primary UI component for user interaction
  */
-  const  Button =({ primary, backgroundColor, size, label, ...props })=> {
-  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+//   const  Button =({ primary, backgroundColor, size, label, ...props })=> {
+//   const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+//   return (
+//     <button
+//       type="button"
+//       className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
+//       style={backgroundColor && { backgroundColor }}
+//       {...props}
+//     >
+//       {label}
+//     </button>
+//   );
+// };
+
+const Button=({variant,disableBoxShadow,disabled,startIcon,endIcon,size,color,label='Default'})=>{
+
   return (
-    <button
-      type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
-      style={backgroundColor && { backgroundColor }}
-      {...props}
-    >
-      {label}
+    <button className={`${variant} ${disableBoxShadow?'no-box-shadow':''} ${size} ${color}`} disabled={disabled}>
+<Icon iconName={startIcon}/>
+{
+  label
+}
+<Icon iconName={endIcon}/>
     </button>
   );
+
 };
-
-// const Button=({variant,boxShadow,disable,startIcon,endIcon,size,color,})=>{
-
-//   return (
-//     <button className={``}></button>
-//   );
-
+// Button.propTypes = {
+//   /**
+//    * Is this the principal call to action on the page?
+//    */
+//   primary: PropTypes.bool,
+//   /**
+//    * What background color to use
+//    */
+//   backgroundColor: PropTypes.string,
+//   /**
+//    * How large should the button be?
+//    */
+//   size: PropTypes.oneOf(['small', 'medium', 'large']),
+//   /**
+//    * Button contents
+//    */
+//   label: PropTypes.string.isRequired,
+//   /**
+//    * Optional click handler
+//    */
+//   onClick: PropTypes.func,
 // };
-Button.propTypes = {
-  /**
-   * Is this the principal call to action on the page?
-   */
-  primary: PropTypes.bool,
-  /**
-   * What background color to use
-   */
-  backgroundColor: PropTypes.string,
-  /**
-   * How large should the button be?
-   */
-  size: PropTypes.oneOf(['small', 'medium', 'large']),
-  /**
-   * Button contents
-   */
-  label: PropTypes.string.isRequired,
-  /**
-   * Optional click handler
-   */
-  onClick: PropTypes.func,
-};
 
-Button.defaultProps = {
-  backgroundColor: null,
-  primary: false,
-  size: 'medium',
-  onClick: undefined,
-};
+// Button.defaultProps = {
+//   backgroundColor: null,
+//   color:'default',
+//   size: 'md',
+//   onClick: undefined,
+// };
 
 export default Button;
--- src/stories/Button.stories.jsx
+++ src/stories/Button.stories.jsx
@@ -3,39 +3,117 @@ import React from 'react';
 import Button  from './Button';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
-export default {
-  title: 'Example/Button',
-  component: Button,
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-  argTypes: {
-      backgroundColor: { control: 'color' },
-  },
-};
+// export default {
+//   title: 'Example/Button',
+//   component: Button,
+//   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+//   argTypes: {
+//       // backgroundColor: { control: 'color' },
+//   },
+// };
+
+export default{
+  title:'Example/Button',
+  component:Button,
+  argTypes:{
+    variant:{
+      options:['default','outline','text'],
+      control:{type:'radio'}
+    },
+    disableBoxShadow:{
+      control:{type:'boolean'}
+    },
+    disabled:{
+      control:{type:'boolean'}
+    },
+    startIcon:{
+      options:['search','lock','visibility','cart','delete'],
+      control:{type:'radio'}
+    },
+    endIcon:{
+      options:['search','lock','visibility','cart','delete'],
+      control:{type:'radio'}
+    },
+    size:{
+      options:['sm','md','lg'],
+      control:{type:'radio'}
+    },
+    color:{
+      options:['default','primary','secondary','danger'],
+      control:{type:'radio'}
+    }
+  }
+}
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+
+
 const Template = (args) => <Button {...args} />;
 
-export const Primary = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
-Primary.args = {
-  primary: true,
-  label: 'Button',
-  children:'default'
+export const Default = Template.bind({});
+Default.args={
+  variant:'default',
+  
+  // children:'default'
 };
 
-export const Secondary = Template.bind({});
-Secondary.args = {
-  label: 'Button',
+export const Outline = Template.bind({});
+Outline.args = {
+  
+  variant:'outline',
+  // children:'default',
+  
 };
 
-export const Large = Template.bind({});
-Large.args = {
-  size: 'large',
-  label: 'Button',
-};
+export const Text = Template.bind({});
+Text.args={
+  variant:'text',
+  
+}
+
+export const DisableBoxShadow = Template.bind({});
+DisableBoxShadow.args={
+disableBoxShadow:true
+}
+
+export const Disabled = Template.bind({});
+Disabled.args={
+  disabled:true,
+  label:'Disabled'
+}
+
+export const StartIcon = Template.bind({});
+StartIcon.args={
+  startIcon:'a'
+}
+
+export const EndIcon = Template.bind({}); 
+EndIcon.args={
+  endIcon:'x'
+}
+
+export const Size = Template.bind({});
+Size.args={
+  size:'md'
+}
+export const Color = Template.bind({});
+Color.args={
+  color:''
+}
 
 // export const Small = Template.bind({});
 // Small.args = {
 //   size: 'small',
+//   label: 'Button',
+// };
+
+// export const Secondary = Template.bind({});
+// Secondary.args = {
+//   label: 'Button',
+// };
+
+// export const Large = Template.bind({});
+// Large.args = {
+//   size: 'large',
 //   label: 'Button',
 // };
